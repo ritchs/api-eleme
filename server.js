5232765c4ceec74ac1882db127f43caa49cf7e1c
Benjamin Glez <ibgm03@gmail.com>
2023-03-05 19:55:38 -0600

    redirect

--- server.js
+++ server.js
@@ -5,39 +5,41 @@ const mongoose = require('mongoose')
 const Usuario = require('./models/userModel')
 app.set('view engine', 'ejs');
 app.set("views", __dirname + "/views");
-app.use( express.static( "public" ) );
+app.use(express.static("public"));
 app.use(express.json());
 
 // rutas
-app.get('/',(req,res)=>{
-res.render("index1", {titulo:"inicio EJS"})
+app.get('/', (req, res) => {
+    res.render("index1", { titulo: "inicio EJS" })
 })
 
-app.get('/grupo',(req,res)=>{
-    res.render("grupo", {titulo:"inicio EJS"})
-    })
+app.get('/grupo', (req, res) => {
+    res.render("grupo", { titulo: "inicio EJS" })
+})
 
 app.get('/form', async (req, res) => {
     console.log("res", req.query)
-    
-   try {
-    const user = await Usuario.create(req.query);
-    res.status(200).json(user);
-   } catch (error) {
-    console.log(error.message)
-    res.status(500).json({message: error.message})
-    
-   }
+
+    try {
+        const user = await Usuario.create(req.query);
+        // res.status(200).json(user);
+            res.redirect('/grupo')
+        
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ message: error.message })
+
+    }
 })
 
-mongoose.connect ('mongodb+srv://ritchs:X3cBt78g89AAL7se@cluster0.kdolw.mongodb.net/blog?retryWrites=true&w=majority')
-//mongoose.connect ('mongodb+srv://admin:admin@cluster0.9msb6zl.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://ritchs:X3cBt78g89AAL7se@cluster0.kdolw.mongodb.net/blog?retryWrites=true&w=majority')
+    //mongoose.connect ('mongodb+srv://admin:admin@cluster0.9msb6zl.mongodb.net/?retryWrites=true&w=majority')
 
     .then(() => {
-    console.log("Conectado a MONGODB")
-    app.listen(3000,()=>{
-        console.log("Node API corriendo en puerto 3000")
+        console.log("Conectado a MONGODB")
+        app.listen(3000, () => {
+            console.log("Node API corriendo en puerto 3000")
+        })
+    }).catch((e) => {
+        console.log(e)
     })
-}).catch((e)=>{
-    console.log(e)
-})
